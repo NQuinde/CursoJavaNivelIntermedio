c7f8bd73a4e1883f634ab7ed892ba704b4a129e8
Nohemí Quinde García <nahomiquinde@gmail.com>
2024-03-06 18:26:29 -0500

    Logica para obtener los caracteres de una palabra

--- src/Repaso/ContarObtenerCaracteres.java
+++ src/Repaso/ContarObtenerCaracteres.java
@@ -4,10 +4,62 @@
  */
 package Repaso;
 
-/**
- *
- * @author Usuario
- */
+import java.util.Scanner;
+
 public class ContarObtenerCaracteres {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        //mayusculas, minusculas, espacios, numeros y letras.
+        String cadena;
+
+        System.out.println("Ingresa una cadena: ");
+        cadena = entrada.nextLine();
+
+        String mayus = "";
+        String minus = "";
+        String numeros = "";
+        String letras = "";
+        String espacios="";
+
+        int n_mayus = 0;
+        int n_minus = 0;
+        int n_numeros = 0;
+        int n_letras = 0;
+        int n_espacios=0;
+     
+
+        for (int i = 0; i < cadena.length(); i++) {
+            if (Character.isLetter(cadena.charAt(i))) {
+               n_letras++;
+               letras += cadena.charAt(i);
+            }
+            
+            if (Character.isUpperCase(cadena.charAt(i))) {
+                n_mayus++;
+                mayus+=cadena.charAt(i);
+            }
+            if (Character.isLowerCase(cadena.charAt(i))) {
+                n_minus++;
+                minus+=cadena.charAt(i);
+            }
+            if (Character.isDigit(cadena.charAt(i))) {
+                n_numeros++;
+                numeros+=cadena.charAt(i);
+            }
+            if (Character.isSpaceChar(cadena.charAt(i))) {
+                n_espacios++;
+                
+            }
+        }
+        System.out.println("Mayusculas("+n_mayus+")"+mayus);
+        System.out.println("Minusculas("+n_minus+")"+minus);
+        System.out.println("Numeros("+n_numeros+")"+numeros);
+        System.out.println("Espacios("+n_espacios+")"+n_espacios);
+        
+        System.out.println(Character.isBmpCodePoint('j'));
+
+    }
 }
+
+
